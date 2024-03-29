75e81b61b8dc4a2e4946f112ea96fa00f60370e9
Simon Garcia <math.garciaprado@gmail.com>
2024-03-29 15:30:13 -0300

    atividade finalizada

--- Aula_1/Exercicio_1.py
+++ Aula_1/Exercicio_1.py
@@ -2,4 +2,22 @@
 
 print("Ciências da Computação - Unicsul")
 
-# ercricio 2
+# Exercicio 2 - Faça um programa em Python que solicite ao usuário asua profissão e mostre na tela a informação digitada
+
+profissao = input("Qual sua profissão?")
+print("Sua profissão é ", profissao)
+
+# Exercicio 3 - Faça um programa em Python que solicite ao usuário asua idade e apresente a informação na tela
+
+idade = input("Quantos anos você tem?")
+print("Você tem", idade, "anos")
+
+# Exercicio 4- Faça um programa em Python que solicite o últimosobrenome do usuário e mostre na tela a mensagem:“Família” e na sequência o sobrenome digitado
+
+sobrenome = input("Qual seu sobrenome?")
+print("sua familia é", sobrenome)
+
+# Exercicio 5 - Faça um programa em Python que solicite o esportefavorito de uma pessoa e apresente-o na tela
+
+esporte = input("qual seu esporte favorito?")
+print("seu esporte favorito é", esporte)
